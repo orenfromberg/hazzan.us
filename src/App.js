c7949c30a48cb6c54db5e73c25159904d1c4c837
Oren Fromberg <oren@adhocteam.us>
2018-01-28 23:29:41 -0500

    Sof Pasuk

--- src/App.js
+++ src/App.js
@@ -1,18 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import audio from './01SofPasuk-MasterPhrase.ogg';
+
+const play = '▶️';
+const stop = '⏹️';
+
+const merkha = () => (
+  <span className="word">
+    מֵרְכָ
+    <span className="mark">&#1445;</span>
+    א
+  </span>
+);
+
+const tipeha = () => (
+  <span className="word">
+    טִפְחָ
+    <span className="mark">&#1430;</span>
+    א
+  </span>  
+);
+
+const sofPasuk = () => (
+  <span className="word">
+    סוֹף&#1470;פָּסוּ<span className="mark">&#1469;</span>ק
+    <span className="mark">&#1475;</span>
+  </span>
+);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      buttonText: play
+    };
+
+    this.toggleAudio = this.toggleAudio.bind(this);
+    this.onAudioEnded = this.onAudioEnded.bind(this);
+  }
+
+  toggleAudio() {
+    if (this.state.buttonText === play) {
+      this.setState({
+        buttonText: stop
+      });
+      this.audio.play();
+    } else {
+      this.setState({
+        buttonText: play
+      });
+      this.audio.load();
+    }
+  }
+
+  onAudioEnded() {
+    this.setState({
+      buttonText: play
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Sof Pasuk/Siluk</h1>
+        <div className="text">
+          {merkha()} {tipeha()} {merkha()} {sofPasuk()}
+        </div>
+        <audio 
+          onEnded={this.onAudioEnded}
+          src={audio} 
+          ref={(input) => this.audio = input }
+          // controls 
+          />
+        <button type="button" onClick={this.toggleAudio}>{this.state.buttonText}</button>
       </div>
     );
   }
